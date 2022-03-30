0cf2019329765cc2cd9b33ee8963a61ba1c1c4d3
Glitch (glitch-blank-website) <none>
2022-03-30 06:03:57 +0000

    🍏🚇 Checkpoint ./script.js:43391257/777

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var pattern = [2,2,4,3,2,1,2,4];
 var progress = 0;
 var gamePlaying = false; 
 var tonePlaying = false;
-var volume;
+var volume = 0.5; 
 
 function startGame() {
   progress =0;
@@ -17,5 +17,31 @@ function stopGame() {
   gamePlaying = false; 
   document.getElementById("stopBtn").classList.remove("hidden");
   document.getElementById("startBtn").classList.add("hidden");
+}
+
+//  Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+
+function startTone (btn) {
+  if (!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.25)
+  }
 }
 
