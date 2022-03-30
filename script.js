e41758183df007d217aaaf2c38c1ccab770190ee
Glitch (glitch-blank-website) <none>
2022-03-30 05:52:36 +0000

    🎲🎥 Checkpoint ./script.js:43391257/371 ./index.html:43391257/127

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 var pattern = [2,2,4,3,2,1,2,4];
 var progress = 0;
 var gamePlaying = false; 
+var tonePlaying = false;
+var volume;
 
-function startGame(){
+function startGame() {
   progress =0;
-  gamePlaying = true;'
+  gamePlaying = true;
 document.getElementById("startBtn").classList.add("hidden");
 document.getElementById("stopBtn").classList.remove("hidden");
 }
 
-function 
+function stopGame() {
+  gamePlaying = false; 
+  document.getElementById("stopBtn").classList.remove("hidden");
+  document.getElementById("startBtn").classList.add("hidden");
+}
+
