fa1a6910a6aaab30f361a61abc3746b5b8c6df5a
Glitch (glitch-blank-website) <none>
2022-03-30 05:41:28 +0000

    📚🏰 Checkpoint ./script.js:43391257/541

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 var pattern = [2,2,4,3,2,1,2,4];
 var progress = 0;
 var gamePlaying = false; 
+
+function startGame(){
+  progress =0;
+  gamePlaying = true;'
+document.getElementById("startBtn").classList.add("hidden");
+document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+function 
